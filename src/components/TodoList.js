4d46a73cc340d83d617c44118df8bcc040cdd71c
warumNicht <andrey_st_minev@yahoo.com>
2020-01-20 23:04:36 +0200

    redux exercise

---
+++ src/components/TodoList.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Todo from "./Todo";
+import { connect } from "react-redux";
+import { getTodosByVisibilityFilter } from "../redux/selectors";
+
+const TodoList = ({ todos }) => (
+  <ul className="todo-list">
+    {todos && todos.length
+      ? todos.map((todo, index) => {
+          return <Todo key={`todo-${todo.id}`} todo={todo} />;
+        })
+      : "No todos, yay!"}
+  </ul>
+);
+
+// const mapStateToProps = state => {
+//   const { byIds, allIds } = state.todos || {};
+//   const todos =
+//     allIds && allIds.length
+//       ? allIds.map(id => (byIds ? { ...byIds[id], id } : null))
+//       : null;
+//   return { todos };
+// };
+
+const mapStateToProps = state => {
+  const { visibilityFilter } = state;
+  const todos = getTodosByVisibilityFilter(state, visibilityFilter);
+  return { todos };
+};
+
+// export default TodoList;
+export default connect(mapStateToProps)(TodoList);
+// export default connect(state => ({todos: getTodos(state) }))(TodoList);
